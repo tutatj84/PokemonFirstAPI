6d3553be7673d79e80c29e470eebc1c2d88c5447
tutatj84 <tiennguyenanh84@gmail.com>
2020-05-16 20:59:50 +0700

    alalal

---
+++ fetchAPI.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', getData);
+
+let getPokemonById = async id => {
+    try {
+        document.getElementById("pokeImg").removeEventListener("click", getData) //not allow user to click when loading
+        document.getElementById("pokeImg").src = "https://cdn.dribbble.com/users/621155/screenshots/2835314/simple_pokeball.gif";
+        let response = await fetch('https://pokeapi.co/api/v2/pokemon/' + id);
+        let resJson = response.json();
+        let data = await resJson;
+
+        resJson.then(() => { //check when load data success to allow user to click
+            document.getElementById("pokeImg").addEventListener("click", getData)
+            document.getElementById("pokeImg").src = "https://pokeres.bastionbot.org/images/pokemon/" + id + ".png"
+            document.getElementById("pokeName").innerText = data.name;
+            console.log(data.name);
+        })
+
+
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function getData() {
+    let idPokemon = Math.floor(Math.random() * 100 + 1);
+    getPokemonById(idPokemon);
+}
